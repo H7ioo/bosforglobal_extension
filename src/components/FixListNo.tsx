4981a659ee8191f204b158413295ca7b1e4c5a14
H7ioo <omar.jano05@gmail.com>
2024-05-15 16:24:54 +0300

    feat: router

---
+++ src/components/FixListNo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FixListNo = ({}) => {
+  return (
+    <div>
+      <input type="number" />
+    </div>
+  );
+};
+
+export default FixListNo;
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,40 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+import { Link, Route, MemoryRouter as Router, Routes } from "react-router-dom";
+import FixListNo from "./components/FixListNo";
 
 const Popup = () => {
-  const [count, setCount] = useState(0);
-  const [currentURL, setCurrentURL] = useState<string>();
-
-  useEffect(() => {
-    chrome.action.setBadgeText({ text: count.toString() });
-  }, [count]);
-
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      setCurrentURL(tabs[0].url);
-    });
-  }, []);
-
-  const changeBackground = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      const tab = tabs[0];
-      if (tab.id) {
-        chrome.tabs.sendMessage(
-          tab.id,
-          {
-            color: "#555555",
-          },
-          (msg) => {
-            console.log("result message:", msg);
-          }
-        );
-      }
-    });
-  };
+  // const [count, setCount] = useState(0);
+  // const [currentURL, setCurrentURL] = useState<string>();
+  //
+  // useEffect(() => {
+  // chrome.action.setBadgeText({ text: count.toString() });
+  // }, [count]);
+  //
+  // useEffect(() => {
+  // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  // setCurrentURL(tabs[0].url);
+  // });
+  // }, []);
+  //
+  // const changeBackground = () => {
+  // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  // const tab = tabs[0];
+  // if (tab.id) {
+  // chrome.tabs.sendMessage(
+  // tab.id,
+  // {
+  // color: "#555555",
+  // },
+  // (msg) => {
+  // console.log("result message:", msg);
+  // }
+  // );
+  // }
+  // });
+  // };
 
   return (
-    <>
-      <ul style={{ minWidth: "700px" }}>
+    <div style={{ width: "200px", height: "300px" }}>
+      {/* <ul style={{ minWidth: "700px" }}>
         <li>Current URL: {currentURL}</li>
         <li>Current Time: {new Date().toLocaleTimeString()}</li>
       </ul>
@@ -44,8 +46,14 @@ const Popup = () => {
       >
         count up
       </button>
-      <button onClick={changeBackground}>change background</button>
-    </>
+      <button onClick={changeBackground}>change background</button> */}
+      <h1>İşlemler</h1>
+      <ul>
+        <li>
+          <Link to="/fix-list-no">Liste Çeki Düzelt</Link>
+        </li>
+      </ul>
+    </div>
   );
 };
 
@@ -53,6 +61,11 @@ const root = createRoot(document.getElementById("root")!);
 
 root.render(
   <React.StrictMode>
-    <Popup />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Popup />}></Route>
+        <Route path="/fix-list-no" element={<FixListNo />}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
